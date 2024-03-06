cc24a6b1d8db7bfb656293a34ee453413902d1ed
giftedpromise <giftedpromise1992@gmail.com>
2024-03-05 20:49:58 -0500

    React

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ const page = (
 
 ReactDOM.render(page, document.getElementById("root"));
 */
+
+/* 1. Why do we need to `import React from "react"` in our files?
+So that we can have access to the global React Dom and to import React library to our JavaScript file. 
+
+2. If I were to console.log(page) in index.js, what would show up?
+JavaScript Object
+
+3. What's wrong with this code:
+```
+const page = (
+    <h1>Hello</h1>
+    <p>This is my website!</p>
+)
+```
+It has 2 parents 
+
+4. What does it mean for something to be "declarative" instead of "imperative"?
+Declarative:
+
+What to do: In declarative programming, you focus on describing the desired outcome or result without specifying the step-by-step instructions or details of how to achieve it.
+Imperative:
+
+How to do it: In imperative programming, you provide explicit instructions on how to perform a task. You outline the step-by-step procedure and control the flow of execution.
+
+5. What does it mean for something to be "composable"?
+n software engineering, "composable" refers to the property of being able to combine smaller, simpler components or building blocks to create more complex systems or functionalities. 
+*/
