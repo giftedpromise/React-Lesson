0080bb76e6a82151a10ee5c5ae99228694b45dc8
giftedpromise <giftedpromise1992@gmail.com>
2024-03-06 04:26:57 +0100

    React

--- script.js
+++ script.js
@@ -164,6 +164,7 @@ Quiz!
 1. What is a React component?
 
 In React, a component is a reusable, self-contained unit of code that defines a specific part of a user interface. 
+A function that returns React elements. (UI)
 
 2. What's wrong with this code?
 ```
@@ -173,7 +174,7 @@ function myComponent() {
     )
 }
 ```
-The function name is supposed to be in pascal notation.
+The function name is supposed to be in pascalcase notation.
 
 3. What's wrong with this code?
 ```
@@ -191,4 +192,110 @@ ReactDOM.render(Header(), document.getElementById("root"))
 ```
 This Header() is supposed to be in this form <Header />
 
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>Reasons I'm excited to learn React</h1>
+        <ol>
+          <li>
+            It's a popular library, so I'll be able to fit in with the cool
+            kids!
+          </li>
+          <li>I'm more likely to get a job as a developer if I know React</li>
+        </ol>
+        <footer>
+          <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
+
+*/
+
+/*
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+       <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol> 
+            
+            </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            
+            <Footer />
+            
+            <MainContent />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
 */
