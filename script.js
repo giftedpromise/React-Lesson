28e420b95aa67c6a401889969a4b4c174d73c897
giftedpromise <giftedpromise1992@gmail.com>
2024-03-14 22:52:21 -0600

    React setup

--- script.js
+++ script.js
@@ -358,3 +358,23 @@ const capitalized = names.map((name) => {
 console.log(capitalized);
 
 */
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+
+const wrappedPokemon = pokemon.map((name) => {
+    return `<p>${name}</p>`
+});
+
+console.log(wrappedPokemon);
+*/
