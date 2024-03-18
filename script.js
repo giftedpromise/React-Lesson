4dc2c8807412b58441bd580fa6a103dea0ea9cbb
giftedpromise <giftedpromise1992@gmail.com>
2024-03-17 21:43:26 -0600

    React setup

--- script.js
+++ script.js
@@ -473,3 +473,22 @@ const capitalizedNames = names.map((name) => {
 console.log(capitalizedNames);
 
 */
+
+/* 
+export default function App() {
+    const colors = [
+            <h3>Red</h3>, 
+            <h3>Orange</h3>, 
+            <h3>Yellow</h3>,
+            <h3>Green</h3>,
+            <h3>Blue</h3>,
+            <h3>Indigo</h3>,
+            <h3>Violet</h3>
+        ]
+    return (
+        <div>
+            {colors}
+        </div>
+    )
+}
+ */
