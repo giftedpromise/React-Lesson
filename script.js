14363f402737656403017668644c23052ee33620
giftedpromise <giftedpromise1992@gmail.com>
2024-03-25 13:03:24 +0100

    Jsx element

--- script.js
+++ script.js
@@ -518,4 +518,26 @@ export default function Hello() {
     </a>
   );
 }
+
+
+import React from "react"
+
+export default function App() {
+    function handleClick() {
+        console.log("I was clicked!")
+    }
+    
+   
+     * Challenge: 
+     * Log something to the console when the mouse hovers over the image
+    
+    
+    return (
+        <div className="container">
+            <img src="https://picsum.photos/640/360" />
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    )
+}
+
 */
