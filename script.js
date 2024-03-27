c8531a9ddb5f8e66f481dec24e3726303cfe90b7
giftedpromise <giftedpromise1992@gmail.com>
2024-03-27 03:34:20 +0100

    React state

--- script.js
+++ script.js
@@ -679,3 +679,35 @@ export default function App() {
 }
 
   */
+
+/* 
+import React, { useState } from "react";
+
+export default function App() {
+    
+     * Challenge: Set up state to track our count (initial value is 0)
+    
+    
+     const [count, setCount] = React.useState(0)
+     
+     function handleMinus() {
+         
+         setCount(count - 1);
+     }
+     
+     function handleplus() {
+         
+         setCount(count + 1);
+     }
+     
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick = {handleMinus}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick = {handleplus}>+</button>
+        </div>
+    )
+}
+ */
