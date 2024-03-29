6d9d47ddf705bed1503b0bebb8c14d3a8af13e18
giftedpromise <giftedpromise1992@gmail.com>
2024-03-28 22:24:30 -0700

    React state

--- script.js
+++ script.js
@@ -779,3 +779,81 @@ const toy = answer ? "teddy bear" : "toy car";
 console.log(toy); // Output will be "teddy bear"
 
 */
+/* 
+import React from "react"
+
+export default function App() {
+
+     * Challenge: Replace the if/else below with a ternary
+     * to determine the text that should display on the page
+    
+    const isGoingOut = false
+    
+    let answer = isGoingOut ? "yes" : "No";   // Use ternary here
+    if(isGoingOut === true) {
+        answer = "Yes"
+    } else {
+        answer = "No"
+    }
+    
+    return (
+        <div className="state">
+            <h1 className="state--title">Do I feel like going out tonight?</h1>
+            <div className="state--value">
+                <h1>{answer}</h1>
+            </div>
+        </div>
+    )
+}
+ */
+
+import React from "react";
+
+export default function App() {
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
+  /**
+   * Challenge:
+   * - Initialize state for `isGoingOut` as a boolean
+   * - Make it so clicking the div.state--value flips that
+   *   boolean value (true -> false, false -> true)
+   * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+   */
+  function changeMind() {
+    setIsGoingOut((prevState) => !prevState);
+  }
+
+  return (
+    <div className="state">
+      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      <div onClick={changeMind} className="state--value">
+        <h1>{isGoingOut ? "Yes" : "No"}</h1>
+      </div>
+    </div>
+  );
+}
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+   
+     * Challenge: See if you can do it all again by yourself :)
+     
+    
+    function addItem() {
+        // Build from scratch :)
+    }
+    
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+    
+    return (
+        <div>
+            <button>Add Item</button>
+            {thingsElements}
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+*/
