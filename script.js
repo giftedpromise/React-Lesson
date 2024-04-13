655f981198dd3a767d9447b6debbe396f855e182
giftedpromise <giftedpromise1992@gmail.com>
2024-04-12 14:05:57 -1200

    state

--- script.js
+++ script.js
@@ -1336,3 +1336,31 @@ const hobbies = ["sports", "cooking", "swimming"];
 for (const hobby of hobbies) {
   console.log(hobby);
 }
+
+/* 
+export function CourseGoal(props) {
+  return (
+    <li>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <h1>Time to Practice</h1>
+      <p>One course, many goals! 🎯</p>
+      <ul>
+       
+        <CourseGoal title = "LEARN REACT" description = "In-depth" />
+         <CourseGoal title ="PRACTICE" description = "Practice working with React components" />
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+*/
