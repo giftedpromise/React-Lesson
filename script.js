03db241b54aef9733d6642d8387d551609337f0e
giftedpromise <giftedpromise1992@gmail.com>
2024-04-13 17:33:36 -1200

    React state

--- script.js
+++ script.js
@@ -1347,7 +1347,21 @@ export function CourseGoal(props) {
   );
 }
 
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <h1>Time to Practice</h1>
+      <p>One course, many goals! 🎯</p>
+      <ul>
+       
+        <CourseGoal title = "LEARN REACT" description = "In-depth" />
+         <CourseGoal title ="PRACTICE" description = "Practice working with React components" />
+      </ul>
+    </div>
+  );
+}
 
+export default App;
 
 */
 
