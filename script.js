cd186538da1b4d0fe90f918e6a10680334aa17d3
giftedpromise <giftedpromise1992@gmail.com>
2024-04-15 23:52:12 -1200

    React state

--- script.js
+++ script.js
@@ -1414,7 +1414,12 @@ export default function App() {
         <div className="box" key={square.id}></div>
     ))
    
- 
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
     
     return (
         <main>
