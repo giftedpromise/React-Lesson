b2c746c62dc9cecd76c561f550515664a8369352
giftedpromise <giftedpromise1992@gmail.com>
2024-04-16 16:38:52 -1200

    React state

--- script.js
+++ script.js
@@ -1477,3 +1477,33 @@ function App() {
 export default App;
 
 */
+
+/*
+export const user = {
+  name: '',
+};
+
+function App() {
+  // Your goal: This function should be called WITH A VALUE for name when the <button> is clicked
+  function handleCreateUser(name) {
+    user.name = name;
+  }
+
+  return (
+    <div id="app">
+      <h1>User Login</h1>
+      <p>
+        <label>Name</label>
+       
+        <input type="text" />
+      </p>
+
+      <p id="actions">
+        <button onClick = {() => handleCreateUser("Max")}>Create User</button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
+*/
