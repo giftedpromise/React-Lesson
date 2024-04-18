03cb83fbef866937d04c704de76ed49b8012357a
giftedpromise <giftedpromise1992@gmail.com>
2024-04-17 21:20:22 -1200

    React state

--- script.js
+++ script.js
@@ -1508,3 +1508,31 @@ function App() {
 
 export default App;
 */
+
+/**
+ import React from "react"
+
+export default function Joke(props) {
+  
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+    
+    
+    const [isShown, setIsShown] = React.useState(false);
+    
+    const toggle = () => {
+        
+    setIsShown(prevIsShown => !prevIsShown)
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            <p>{props.punchline}</p>
+            <button onClick={toggle}>Toggle</button>
+            <hr />
+        </div>
+    )
+}
+
+ */
